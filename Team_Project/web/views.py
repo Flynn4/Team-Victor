5b3a3b0c04b31811998df6d97a00ad6bfc46fda7
Flynn4 <hmf245388320@qq.com>
2020-03-11 01:58:22 +0000

    Update Login and Registration form

--- Team_Project/web/views.py
+++ Team_Project/web/views.py
@@ -8,20 +8,21 @@ from .models import *
 def index(request):
 
     # Add all details into dict
-    games_dict = {}
+    dict = {}
     games = Game.objects.all()
-    games_dict['games'] = Game.objects.all()
+    dict['games'] = Game.objects.all()
+    dict['categories'] = Category.objects.all()
 
     # Show 5 games on home page
     five_games = []
     gamesId = random.sample(range(0, len(games)), 5)
     for i in gamesId:
         five_games.append(games[i])
-    games_dict['five_games'] = five_games
+    dict['five_games'] = five_games
 
     # Random show video from database
     videos = Video.objects.all()
     i = random.randint(0, len(videos) - 1)
-    games_dict['video'] = videos[i]
+    dict['video'] = videos[i]
 
-    return render(request, "web/index.html", games_dict)
+    return render(request, "web/index.html", dict)
